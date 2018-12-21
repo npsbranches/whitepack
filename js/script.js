892e565bfc8d83f76c55284cb4b7a4805344fefe
npsbranches <45974239+npsbranches@users.noreply.github.com>
2018-12-21 16:19:49 +0500

    2.0
    Добавлен слайдер в блок О нас

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(window).resize(function () {
             var the_top = $(document).scrollTop();
             if (the_top > 90) {
                 $('.menu').slideUp();
-            } 
+            }
         });
     }
 })
@@ -45,26 +45,25 @@ $(function () {
     });
 })
 
-$('#btn-order').click(function() {
+$('#btn-order').click(function () {
     $('.order-modal').toggleClass('active');
 })
-$('.btn-buy').click(function() {
-    $('.order-modal').toggleClass('active');
-})
-
-$('.order-modal-bg').click(function() {
+$('.btn-buy').click(function () {
     $('.order-modal').toggleClass('active');
 })
 
-$('.order-modal-close').click(function() {
+$('.order-modal-bg').click(function () {
     $('.order-modal').toggleClass('active');
 })
 
-// $('.hero').parallax({
-//     imageSrc: 'images/hero.jpg',
-//     androidFix: true,
-//     iosFix: true,
-//     zIndex: 1,
-//     naturalHeight: 500
+$('.order-modal-close').click(function () {
+    $('.order-modal').toggleClass('active');
+})
 
-// });
+$('.about-slider-wrapper').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    animateOut: 'fadeOut'
+});
